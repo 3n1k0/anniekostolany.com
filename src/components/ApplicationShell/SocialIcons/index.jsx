3e361916f63416b72eb4e4d51c3ed1f502087fc3
Andor Polgar <hngrhorace@gmail.com>
2017-10-23 22:04:42 +0200

    open facebook app

--- src/components/ApplicationShell/SocialIcons/index.jsx
+++ src/components/ApplicationShell/SocialIcons/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {socialIcons} from './style.scss';
 
+const getFacebookUrl = ua => (
+	(/iPhone|iPad|iPod/).test(ua) ? 'fb://page?id=1389097571179740' : 'https://facebook.com/anniekostolany'
+);
+
+const getInstagramUrl = ua => (
+	(/iPhone|iPad|iPod/).test(ua) ? 'instagram://user?username=anniekostolany' : 'https://instagram.com/anniekostolany'
+);
+
 export default () => (
 	<div className={socialIcons}>
-		<a href="https://instagram.com/anniekostolany" target="_blank"><i className="fa fa-instagram"></i></a>
-		<a href="https://facebook.com/anniekostolany" target="_blank"><i className="fa fa-facebook-official"></i></a>
+		<a href={getInstagramUrl(window.navigator.userAgent)} target="_blank"><i className="fa fa-instagram"></i></a>
+		<a href={getFacebookUrl(window.navigator.userAgent)} target="_blank"><i className="fa fa-facebook-official"></i></a>
 	</div>
 );
