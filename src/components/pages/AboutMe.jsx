8f42273a80a2c2e049afc3cb078d05a1f3ae5c85
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-24 23:49:14 +0200

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -8,16 +8,16 @@ export default () => (
 
 		<p>Hello!</p>
 <p>&nbsp;</p>
-<p>My name is Annie, I am a Hungarian girl living in Amsterdam. I take photos and and I&rsquo;m in love with it.</p>
+<p>My name is Annie, I am a Hungarian girl living in Amsterdam. I take photos and and I'm in love with it.</p>
 <p>I studied literature, which taught me to express my thoughts but I have always been struggled with expressing my feelings. <br />I tried different forms of writing but I always felt somehow limited.</p>
 <p>It was not the language, it was me. I simply needed something else. <br /> <br />I discovered photography in the beginning of 2017 and I immediately knew I found what I was looking for. What you see in my portfolio is my progress, which is still ongoing. I work hard every day and I am really focused on becoming a better photographer.</p>
 <p>If I had to describe my personality, I would say I am highly sensitive. <br />I laugh too much, talk crazy things, sometimes cry at the news (or just because...) , and I am always ready to make fun of anything, mostly myself. <br />I want to adopt all the orphan children and save all the stray animals. But, and I know it might be weird, at the same time I am really outspoken, critical and sarcastic.</p>
-<p>I appreciate life, all the beauty that surrounds us. This is where my inspiration comes from. <br />I believe that everybody deserves to be loved. Maybe by now you understand why I am so attracted to couple shoots and wedding photography.</p>
+<p>I appreciate life, all the beauty that surrounds us. This is where my inspiration comes from. <br />I believe that everybody deserves to be loved. Maybe by now you get the idea why I am so attracted to couple shoots and wedding photography.</p>
 <p>&nbsp;</p>
 <p>In the past few months I have been travelling.</p>
 <p>I took photos in Germany, Hungary, France, the Netherlands, and in the States. <br />No matter where I am, I always try to absorb as much as I can. <br /> Not only because I think my photography benefits from everything I experience, but also because I have MAJOR wanderlust. Foreign cultures and all corners of this world fascinate me more than anything.</p>
 <p>The genres I enjoy the most are portraiture and love shoots, but I would love to challenge myself as an event photographer too.</p>
-<p>For all&nbsp;enquires don&rsquo;t hesitate to email me at info@anniekostolany.com or by filling out the contact form.</p>
+<p>For all enquires don't hesitate to email me at info@anniekostolany.com or by filling out the contact form.</p>
 <p>&nbsp;&nbsp;</p>
 		<BookNowButton />
 	</TextPage>
