4163465187a0660a3d7d540bbd1eae4b545ed1f1
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-26 17:19:22 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,11 +3,13 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
-		<Photo file="home/36535016801_4bb52a0350_k.jpg"/>
-		<Photo file="home/37391781721_de9fa7bc64_k.jpg"/>
-		<Photo file="home/35940475432_a8b639f479_k.jpg"/>
-		<Photo file="home/35269214104_333f4411cc_k.jpg"/>
-		<Photo file="home/35269204374_c8de3c23cd_k.jpg"/>
-		<Photo file="home/36570079976_e096760b47_k.jpg"/>
+		<Photo file="home/zsofi.jpg"/> 
+		<Photo file="home/jillmermaid.jpg"/>
+		<Photo file="home/anna.jpg"/> 
+		<Photo file="home/stephanie.jpg"/>
+		<Photo file="home/zsofia.jpg"/> 
+		<Photo file="home/zsofifuveszkert.jpg"/> 
+		<Photo file="home/evi.jpg"/> 
+		<Photo file="home/jillsunset.jpg"/> 
 	</AlbumPage>
 );
