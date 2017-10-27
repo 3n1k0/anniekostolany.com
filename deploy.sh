0b7235312d5ab32d888317c2debfec4d32a6aa72
Andor Polgar <hngrhorace@gmail.com>
2017-10-27 22:19:53 +0200

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd out
 for f in ./*
 do
-  curl --ftp-create-dirs -T $f -u $FTP_USER:$FT_PASS ftp://anniekostolany.com/public_html/$f
+  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://anniekostolany.com/public_html/$f
 done
