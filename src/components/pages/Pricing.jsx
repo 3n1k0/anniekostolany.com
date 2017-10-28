92ebe7827fe9b6aceaec37948d4ffcf0991790e7
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 22:38:19 +0200

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -4,35 +4,36 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<TextPage>
-		<h2>Pricing</h2>
-<h3><span style="color: #ff0000;">SALE!</span></h3>
-<p>All portrait sessions booked until 17th November (couple, family, baby and fashion) are discounted by 50%.</p>
-<p>Regular price for these sessions: 180 EUR + VAT</p>
-<p>Discounted price: <strong>90 EUR + VAT</strong></p>
-<p>For this you get:</p>
+		
+<h2>Rates</h2>
+
+<p>All portrait sessions booked until 17th November (couple, family and baby) are discounted by 50%.</p>
+
+
+<p>Portrait sessions: 180 Eur</p>
+		
+<p>A session includes:</p>
+
 <ul>
-<li>One full-hour professional photoshoot</li>
-<li>25 edited, high resolution images</li>
+<li>90 minutes shooting</li>
+<li>25 edited, high resolution images of your choice</li>
 <li>Access to password-protected online gallery</li>
 </ul>
-<p>&nbsp;</p>
-<h3>So how does this work?</h3>
-<p>First of all, you need to contact me with your idea.&nbsp;</p>
-<p>We are going talk about the location, the clothes, the makeup and the possible props.&nbsp;</p>
-<p>It's ok if you haven't figured out everything yet, I can absolutely help you with the planning.</p>
-<p>When we agree about everything, we pick a time and a place where we meet.</p>
-<p>The photoshoot will take one hour and we will be having a lot of fun!</p>
-<p><br /> In 3-4 days I will send you a selection of preview images (not the full resolution yet). <br /> From these, you will be able to pick 25 of your favourite photos, which I`ll edit to perfection.&nbsp;</p>
-<p>When I'm ready, I email you the instructions about accessing the online gallery from where you can download the retouched photos in full resolution.&nbsp;</p>
+		
+
+
+<p>Workflow</p>
+<p>First of all, contact me with your idea. We will discuss the location, the clothes I recommend, the makeup and the possible props.
+It's ok if you haven't figured out everything yet, I can help you with the planning.</p>
+<p>The photoshoot will take 1-1,5 hour and we will be having a lot of fun!</p>
+<p>In 3-4 days I will send you a selection of preview images (not the full resolution yet). From these, you will be able to pick 25 of your favourite photos, which I`ll edit to perfection in my signature style.</p>
 <p>It's really quick and easy.</p>
 <p>If you have any more questions, feel free to email me at info@anniekostolany.com or by filling out the contact form.</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>Please keep in mind, that delivery time can take up to 14 days. <br />In case of urgent enquires please contact me for express rates.&nbsp;</p>
-<p><br /> Event photography is excluded from promotion. All events are individually priced based on location, number of people and length of the period you need me to be there.</p>
-<p>&nbsp;</p>
+
+<p>Please keep in mind, that delivery time can take up to 14 days. </p>
+In case of urgent enquires please contact me for express rates.</p>
+
+	<p>Event photography is excluded from promotion. All events are individually priced based on location, number of people and length of the period you need me to be there.</p>
 <p>50% discount is only valid for bookings made until 17. 11.</p>
 		<BookNowButton />
 	</TextPage>
