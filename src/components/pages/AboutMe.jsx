9f29baa76210982721ed01a464867420f4131ba1
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 22:24:48 +0200

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -14,9 +14,9 @@ It was not the language, it was me. I simply needed something else.</p>
 <p>I discovered photography in the beginning of 2017 and I immediately knew I found what I was looking for. What you see in my portfolio is my progress, which is still ongoing. I work hard every day on becoming a better photographer.</p>
 <p>If I had to describe my personality, I would say I am highly sensitive. I laugh too much, talk crazy things, sometimes cry at the news (or just because...) , and I am always ready to make fun of anything, mostly myself. I would adopt all the orphan children and save all the stray animals. But, and I know it might be weird, at the same time I am really outspoken, critical and sarcastic.</p>
 <p>I appreciate life, all the beauty that surrounds us. This is where my inspiration comes from. <br />I believe that everybody deserves to be loved. Maybe by now you get the idea why I am so attracted to couple shoots and wedding photography.</p>
-<p>In the past few months I have been travelling.</p>
-<p>I took photos in Germany, Hungary, France, the Netherlands, and in the States. No matter where I am, I always try to absorb as much as I can. Not only because I think my photography benefits from everything I experience, but also because I have MAJOR wanderlust. Foreign cultures and all corners of this world fascinate me more than anything.</p>
-<p>The genres I enjoy the most are portraiture and love shoots, but I would love to challenge myself as an event photographer too.</p>
+<p>In the past few months I have been travelling.
+I took photos in Germany, Hungary, France, the Netherlands, and in the States. No matter where I am, I always try to absorb as much as I can. Not only because I think my photography benefits from everything I experience, but also because I have MAJOR wanderlust. Foreign cultures and all corners of this world fascinate me more than anything.
+The genres I enjoy the most are portraiture and love shoots, but I would love to challenge myself as an event photographer too.</p>
 <p>For all enquires don't hesitate to email me at info@anniekostolany.com or by filling out the contact form.</p>
 		<BookNowButton />
 	</TextPage>
