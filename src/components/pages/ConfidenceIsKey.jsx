81e859cd55e02848d7d5aabe1c62ce4a33f991b5
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 14:06:40 +0200

    Update ConfidenceIsKey.jsx

--- src/components/pages/ConfidenceIsKey.jsx
+++ src/components/pages/ConfidenceIsKey.jsx
@@ -7,6 +7,7 @@ export default () => (
 		<Photo file="confidence_is_key/35586955786_d53cbfb25c_k.jpg"/>
 		<Photo file="confidence_is_key/35586951886_3a306fad3a_k.jpg"/>
 		<Photo file="confidence_is_key/35586950946_0e0171d85f_k.jpg"/>
+		<Photo file="confidence_is_key/matiasorange.jpg"/>
 		<Photo file="confidence_is_key/35586946126_c9067a803e_k.jpg"/>
 		<Photo file="confidence_is_key/35458333832_6494e862d1_k.jpg"/>
 	</AlbumPage>
