9610f3354c128627b3a355ceb6d8f18eabb64087
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 18:19:47 +0200

    Update StreetStyle.jsx

--- src/components/pages/StreetStyle.jsx
+++ src/components/pages/StreetStyle.jsx
@@ -3,5 +3,27 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
+		<Photo file="streetsyle/smink.jpg"/>
+		<Photo file="streetsyle/utszele.jpg"/>
+		<Photo file="streetsyle/parkolohaz.jpg"/>
+		<Photo file="streetsyle/parkolo.jpg"/>
+		<Photo file="streetsyle/oszlop.jpg"/>
+		<Photo file="streetsyle/guggolos.jpg"/>
+		<Photo file="streetsyle/piros.jpg"/>
+		<Photo file="streetsyle/mozaik.jpg"/>
+		<Photo file="streetsyle/mozaikkozel.jpg"/>
+		<Photo file="streetsyle/hajszal.jpg"/>
+		<Photo file="streetsyle/anker2.jpg"/>
+		<Photo file="streetsyle/anker1.jpg"/>
+		<Photo file="streetsyle/anker3.jpg"/>
+		<Photo file="streetsyle/anker4.jpg"/>
+		<Photo file="streetsyle/zold.jpg"/>
+		<Photo file="streetsyle/zold2.jpg"/>
+		<Photo file="streetsyle/flare1.jpg"/>
+		<Photo file="streetsyle/flare2.jpg"/>
+		<Photo file="streetsyle/flare3.jpg"/>
+		<Photo file="streetsyle/flare4.jpg"/>
+	
+		
 	</AlbumPage>
 );
