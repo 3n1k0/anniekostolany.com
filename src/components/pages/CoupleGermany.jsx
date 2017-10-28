9b2053eb98dee1b1d13fb402d5927ab8baac297a
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 19:41:22 +0200

    Update CoupleGermany.jsx

--- src/components/pages/CoupleGermany.jsx
+++ src/components/pages/CoupleGermany.jsx
@@ -3,7 +3,9 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
+		<Photo file="couple_shoots/pled.jpg"/>
 		<Photo file="couple_shoots/35197522211_52bbdc3a5f_k.jpg"/>
+		<Photo file="couple_shoots/pipacs.jpg"/>
 		<Photo file="couple_shoots/35327296405_ced9926185_k.jpg"/>
 		<Photo file="couple_shoots/34939741490_6f1c0bc417_k.jpg"/>
 		<Photo file="couple_shoots/35197522641_a8a3b05128_k.jpg"/>
