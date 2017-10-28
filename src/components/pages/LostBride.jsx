143830febb93bb672c176c8e06092058d7d4336e
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 17:13:17 +0200

    Update LostBride.jsx

--- src/components/pages/LostBride.jsx
+++ src/components/pages/LostBride.jsx
@@ -3,12 +3,12 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
-		<Photo file="lost_bride/35838537094_69f2c806b0_k.jpg"/>
-		<Photo file="lost_bride/35838556994_091d59ea9a_k.jpg"/>
-		<Photo file="lost_bride/35864164163_7169c2a522_k.jpg"/>
-		<Photo file="lost_bride/36504062342_c7ff65c047_k.jpg"/>
-		<Photo file="lost_bride/36535024201_70466d7ea9_k.jpg"/>
-		<Photo file="lost_bride/36535025851_b4b07895c4_k.jpg"/>
-		<Photo file="lost_bride/36673517345_7e59ff0e82_k.jpg"/>
+		<Photo file="lost_bride/mermaid.jpg"/>
+		<Photo file="lost_bride/csokornezo.jpg"/>
+		<Photo file="lost_bride/lookingback.jpg"/>
+		<Photo file="lost_bride/backlit.jpg"/>
+		<Photo file="lost_bride/shadow.jpg"/>
+		<Photo file="lost_bride/sunset.jpg"/>
+		<Photo file="lost_bride/szep.jpg"/>
 	</AlbumPage>
 );
