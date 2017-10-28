484e12849bb4b49e389895bb061986b7904c199a
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 17:23:18 +0200

    Update LostBride.jsx

--- src/components/pages/LostBride.jsx
+++ src/components/pages/LostBride.jsx
@@ -6,8 +6,8 @@ export default () => (
 		<Photo file="lost_bride/mermaid.jpg"/>
 		<Photo file="lost_bride/lookingback.jpg"/>
 		<Photo file="lost_bride/csokornezo.jpg"/>
-		<Photo file="lost_bride/backlit.jpg"/>
 		<Photo file="lost_bride/shadow.jpg"/>
+		<Photo file="lost_bride/backlit.jpg"/>
 		<Photo file="lost_bride/sunset.jpg"/>
 		<Photo file="lost_bride/szep.jpg"/>
 	</AlbumPage>
