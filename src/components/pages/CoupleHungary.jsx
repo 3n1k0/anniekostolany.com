8ccc16235a08960e33b1ff7104f5df001a4e9bad
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 19:57:40 +0200

    Update CoupleHungary.jsx

--- src/components/pages/CoupleHungary.jsx
+++ src/components/pages/CoupleHungary.jsx
@@ -3,9 +3,8 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
-		<Photo file="couple_shoots_hun/1.jpg"/>
-		<Photo file="couple_shoots_hun/2.jpg"/>
 		<Photo file="couple_shoots_hun/3.jpg"/>
+		<Photo file="couple_shoots_hun/2.jpg"/>
 		<Photo file="couple_shoots_hun/4.jpg"/>
 		<Photo file="couple_shoots_hun/5.jpg"/>
 		<Photo file="couple_shoots_hun/6.jpg"/>
