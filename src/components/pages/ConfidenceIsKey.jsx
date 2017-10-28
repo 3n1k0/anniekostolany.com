008316d44d0eb2c123b00ecd9a732ac48414e460
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 14:23:50 +0200

    Update ConfidenceIsKey.jsx

--- src/components/pages/ConfidenceIsKey.jsx
+++ src/components/pages/ConfidenceIsKey.jsx
@@ -3,12 +3,12 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
-		<Photo file="confidence_is_key/35586955786_d53cbfb25c_k.jpg"/>
-		<Photo file="confidence_is_key/35586958476_1e7e614ada_k.jpg"/>
-		<Photo file="confidence_is_key/35586951886_3a306fad3a_k.jpg"/>
-		<Photo file="confidence_is_key/35586950946_0e0171d85f_k.jpg"/>
-		<Photo file="confidence_is_key/35586946126_c9067a803e_k.jpg"/>
-		<Photo file="confidence_is_key/35458333832_6494e862d1_k.jpg"/>
-		<Photo file="confidence_is_key/35496032271_63c250b8cb_k.jpg"/>
+		<Photo file="confidence_is_key/viragok.jpg"/>
+		<Photo file="confidence_is_key/racsok.jpg"/>
+		<Photo file="confidence_is_key/hmcs1.jpg"/>
+		<Photo file="confidence_is_key/hmcs2.jpg"/>
+		<Photo file="confidence_is_key/matiasorange.jpg"/>
+		<Photo file="confidence_is_key/tamaszkodos.jpg"/>
+		<Photo file="confidence_is_key/legjobb.jpg"/>
 	</AlbumPage>
 );
