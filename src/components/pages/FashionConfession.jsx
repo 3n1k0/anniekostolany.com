8d1945cde7358f1ae98f3d1fedc6a788f93f9dd8
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 14:12:27 +0200

    Update FashionConfession.jsx

--- src/components/pages/FashionConfession.jsx
+++ src/components/pages/FashionConfession.jsx
@@ -3,14 +3,12 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
-		<Photo file="fashion_confession/36478586491_c53edc1c09_k.jpg"/>
-		<Photo file="fashion_confession/35781335614_721dbe8af5_k.jpg"/>
-		<Photo file="fashion_confession/36570077676_32dd452e72_k.jpg"/>
-		<Photo file="fashion_confession/36570079976_e096760b47_k.jpg"/>
-		<Photo file="fashion_confession/36447606892_42128b93ec_k.jpg"/>
-		<Photo file="fashion_confession/36219794250_1d9fac489a_k.jpg"/>
-		<Photo file="fashion_confession/36616573355_b72cd7ea4f_k.jpg"/>
-		<Photo file="fashion_confession/35807127273_fbaabcdbde_k.jpg"/>
-		<Photo file="fashion_confession/35781360124_ecf72fcbfb_k.jpg"/>
+		<Photo file="fashion_confession/annasand.jpg"/>
+		<Photo file="fashion_confession/annasand1.jpg"/>
+		<Photo file="fashion_confession/oszlopok.jpg"/>
+		<Photo file="fashion_confession/lepcso.jpg"/>
+		<Photo file="fashion_confession/kerites.jpg"/>
+		<Photo file="fashion_confession/yellow1.jpg"/>
+		<Photo file="fashion_confession/yellow2.jpg"/>
 	</AlbumPage>
 );
