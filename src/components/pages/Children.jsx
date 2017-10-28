3c77d9502bdeeea45ffdabf9c80f26ab5c9544b5
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 20:54:31 +0200

    Update Children.jsx

--- src/components/pages/Children.jsx
+++ src/components/pages/Children.jsx
@@ -3,11 +3,11 @@ import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
 	<AlbumPage>
+		<Photo file="children/35804680386_91d0cde856_k.jpg"/>
+		<Photo file="children/zoe.jpg"/>
 		<Photo file="children/36800976664_cf99bb3aa6_k.jpg"/>
 		<Photo file="children/36800976784_beca9f8607_k.jpg"/>
 		<Photo file="children/37511184711_6f927e6cad_k.jpg"/>
-		<Photo file="children/35804680386_91d0cde856_k.jpg"/>
-		<Photo file="children/zoe.jpg"/>
 		<Photo file="children/35713356761_96a8c6af94_k.jpg"/>
 		<Photo file="children/35457314120_e536be26ac_k.jpg"/>
 		<Photo file="children/35035613503_7d219ee480_k.jpg"/>
