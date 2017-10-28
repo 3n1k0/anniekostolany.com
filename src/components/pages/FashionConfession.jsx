761e244aeb4ad6b5836b6792940cfc0990588e8b
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-28 16:47:53 +0200

    Update FashionConfession.jsx

--- src/components/pages/FashionConfession.jsx
+++ src/components/pages/FashionConfession.jsx
@@ -5,9 +5,11 @@ export default () => (
 	<AlbumPage>
 		<Photo file="fashion_confession/annasand.jpg"/>
 		<Photo file="fashion_confession/annasand1.jpg"/>
+		<Photo file="fashion_confession/fuggony.jpg"/>
 		<Photo file="fashion_confession/oszlopok.jpg"/>
 		<Photo file="fashion_confession/lepcso.jpg"/>
 		<Photo file="fashion_confession/kerites.jpg"/>
+		<Photo file="fashion_confession/mezitlab.jpg"/>
 		<Photo file="fashion_confession/yellow1.jpg"/>
 		<Photo file="fashion_confession/yellow2.jpg"/>
 	</AlbumPage>
