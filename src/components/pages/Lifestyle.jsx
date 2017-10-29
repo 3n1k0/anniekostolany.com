28970fd073dc7c73578e266391843c16a19ee200
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-29 17:29:34 +0100

    Update Lifestyle.jsx

--- src/components/pages/Lifestyle.jsx
+++ src/components/pages/Lifestyle.jsx
@@ -11,7 +11,7 @@ export default () => (
 
 		<p>Do you have a beautiful family and super cute kids? Let's take some photos of them!</p> <p>The point of lifestyle photography is to capture the happiest everyday moments without too much stressing about planning the shoot.
 			Let's meet in the cosiness of your home or at any other place chosen by you. Picking the location shows your style, your unique taste, your way of living. And what is the most important: it tells your story.
-		You will have nothing to do, except having fun with the people you love. This way I can promise that your photos will turn out to be really candid and natural.</p>
+		<br>You will have nothing to do, except having fun with the people you love. This way I can promise that your photos will turn out to be really candid and natural.</p>
 
 		<BookNowButton />
 	</TextPage>
