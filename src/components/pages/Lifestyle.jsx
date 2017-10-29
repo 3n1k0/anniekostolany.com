5b0a3f4c838132f9ca0c101c2d1557318e9dcdd9
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-10-29 17:04:54 +0100

    Update Lifestyle.jsx

--- src/components/pages/Lifestyle.jsx
+++ src/components/pages/Lifestyle.jsx
@@ -9,11 +9,9 @@ export default () => (
 
 		<p>Check out <Link to="/pricing">my rates</Link> to see the amazing deal I figured out for you to make this fall really special!</p>
 
-		<p>Do you have a beautiful family and super cute kids?  Let's take some photos of them!</p>
-
-		<p>I, personally always get intimidated by studio shoots, and I truly believe that they are not so very personal. The environment doesn't help you to fully express your personality and posing can be unnatural for someone who's not used to it.</p>
-
-		<p>Instead of all this fuss, let’s gather your family and have some hot chocolate together! <br /> We can either meet in the safety of your home or walk together to a nice cafe place in Amsterdam. <br /> You will have nothing to do, except having fun with the people you love. <br /> This way I can promise that your photos will be truly candid and natural.</p>
+		<p>Do you have a beautiful family and super cute kids? Let's take some photos of them! The point of lifestyle photography is to capture the happiest everyday moments without too much stressing about planning the shoot.
+			Let's meet in the cosiness of your home or at any other location chosen by you. This will show your taste, your style, your way of living. It tells your story.
+		You will have nothing to do, except having fun with the people you love. This way I can promise that your photos will turn out to be really candid and natural.</p>
 
 		<BookNowButton />
 	</TextPage>
