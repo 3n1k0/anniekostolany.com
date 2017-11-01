c0aee89615cad5c6e09c02747711ceca8a2c2dba
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-11-01 11:04:38 +0100

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ export default () => (
 		<h2>About me</h2>
 
 		<p>Hello!</p>
-		<p>My name is Annie, I am a Hungarian girl living in Amsterdam. I take photos and and I'm in love with it.</p>
+		<p>My name is Annie, I am a Hungarian girl living in Haarlem, Netherlands. I take photos and and I'm in love with it.</p>
 		<p>I studied literature, which taught me to express my thoughts but I have always been struggled with expressing my feelings. I tried different forms of writing but I always felt somehow limited. It was not the language, it was me. I simply needed something else.</p> 
 		<p>I discovered photography in the beginning of 2017 and I immediately knew I found what I was looking for. What you see in my portfolio is my progress, which is still ongoing. I work hard every day on becoming a better photographer.</p>
 		<p>If I had to describe my personality, I would say I am highly sensitive. I laugh too much, talk crazy things, sometimes cry at the news (or just because...) , and I am always ready to make fun of anything, mostly myself. I would adopt all the orphan children and save all the stray animals. But, and I know it might be weird, at the same time I am really outspoken, critical and sarcastic.</p>
