33404074a2596443b2d76208bb70bca89df4fd05
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-11-04 14:31:54 +0100

    Update Children.jsx

--- src/components/pages/Children.jsx
+++ src/components/pages/Children.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { AlbumPage, Photo } from './AlbumPage';
 
 export default () => (
+	<TextPage>
+		<h2>Holiday Special </h2>
+
+		<p>Would you like to have a nice Christmas card with your kids that you can send to every family member? Read more about my amazing Haarlem Holiday Special <a href="/pricing">here</a></p>!
+		
+	
+	</TextPage>
 	<AlbumPage>
+		<Photo file="children/kiskutyas.jpg"/>
 		<Photo file="children/35804680386_91d0cde856_k.jpg"/>
 		<Photo file="children/zoe.jpg"/>
 		<Photo file="children/36800976664_cf99bb3aa6_k.jpg"/>
