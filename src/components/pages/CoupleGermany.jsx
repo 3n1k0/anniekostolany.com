96b710bfddd63f4ed2d708537450fbc97770bdd6
Andor Polgar <hngrhorace@gmail.com>
2017-11-05 22:25:03 +0100

    add alt prop

--- src/components/pages/CoupleGermany.jsx
+++ src/components/pages/CoupleGermany.jsx
@@ -4,14 +4,14 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
-		<Photo file="couple_shoots/pled.jpg"/>
-		<Photo file="couple_shoots/35197522211_52bbdc3a5f_k.jpg"/>
-		<Photo file="couple_shoots/pipacs.jpg"/>
-		<Photo file="couple_shoots/35327296405_ced9926185_k.jpg"/>
-		<Photo file="couple_shoots/34939741490_6f1c0bc417_k.jpg"/>
-		<Photo file="couple_shoots/35197522641_a8a3b05128_k.jpg"/>
-		<Photo file="couple_shoots/35197523791_4e180460af_k.jpg"/>
-		<Photo file="couple_shoots/35197525161_cc09a2300d_k.jpg"/>
+		<Photo file="couple_shoots/pled.jpg" />
+		<Photo file="couple_shoots/35197522211_52bbdc3a5f_k.jpg" />
+		<Photo file="couple_shoots/pipacs.jpg" />
+		<Photo file="couple_shoots/35327296405_ced9926185_k.jpg" />
+		<Photo file="couple_shoots/34939741490_6f1c0bc417_k.jpg" />
+		<Photo file="couple_shoots/35197522641_a8a3b05128_k.jpg" />
+		<Photo file="couple_shoots/35197523791_4e180460af_k.jpg" />
+		<Photo file="couple_shoots/35197525161_cc09a2300d_k.jpg" alt="Couple holding hands" />
 		<BookNowButton />
 	</Page>
 );
