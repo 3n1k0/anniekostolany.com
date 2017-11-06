ea4925f10792a366502a742a074a632a0f54dab2
andormade <hngrhorace@gmail.com>
2017-11-06 19:22:22 +0100

    fix eslint errors

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -4,61 +4,50 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
+		<h2>Rates</h2>
 
-<h2>Rates</h2>
+		<p>All portrait sessions booked until 30th November (excl. mini sessions) are discounted by 50% from the prices below.</p>
 
-<p>All portrait sessions booked until 30th November (excl. mini sessions) are discounted by 50% from the prices below.</p>
+		<h4>Love / pre-wedding shoot: 180 Eur</h4>
+		<ul>
+			<li>Perfect for celebrating your engagement, your friendship</li>
+			<li>Or just because</li>
+			<li>Best to do it on-location</li>
+			<li>90 minutes, 30 beautifully edited photos</li>
+			<li>Including 5 classic and elegant black and white</li>
+			<li>You receive 1 photo on the same day that you can post instantly to social media</li>
+			<li>All photos will be delivered to your own password-protected online gallery</li>
+		</ul>
 
-
-<h4>Love / pre-wedding shoot: 180 Eur</h4>
-
-<ul>
-<li>Perfect for celebrating your engagement, your friendship</li>
-<li>Or just because</li>
-<li>Best to do it on-location</li>
-<li>90 minutes, 30 beautifully edited photos</li>
-<li>Including 5 classic and elegant black and white</li>
-<li>You receive 1 photo on the same day that you can post instantly to social media</li>
-<li>All photos will be delivered to your own password-protected online gallery</li>
-
-</ul>
-
-
-<h4>Family portrait session: 180 Eur</h4>
-
-<ul>
-<li>Perfect for any family occasions, or for a relaxed Sunday afternoon</li>
-<li>Can be done on-location and in your own home (wherever you think it suits you better)</li>
-<li>I spend 1,5 hours with your family and will capture your best moments</li>
-<li>You receive 30 beautifully edited photos including 1 same-day photo</li>
-<li>All photos will be delivered to your own password-protected online gallery</li>
-</ul>
+		<h4>Family portrait session: 180 Eur</h4>
+		<ul>
+			<li>Perfect for any family occasions, or for a relaxed Sunday afternoon</li>
+			<li>Can be done on-location and in your own home (wherever you think it suits you better)</li>
+			<li>I spend 1,5 hours with your family and will capture your best moments</li>
+			<li>You receive 30 beautifully edited photos including 1 same-day photo</li>
+			<li>All photos will be delivered to your own password-protected online gallery</li>
+		</ul>
 
 
-<h4>Children Mini Holiday Special: 80 Eur</h4>
-<ul>
-<li>Great as Christmas card for grandmas, grandpas, and any family members or friends</li>
-<li>Christmas-styled (fairy lights, cute decoration)</li>
-<li>In my home-studio in Haarlem, really close to the train station</li>
-<li>30 minutes, real quick </li>
-<li>10 edited images</li>
-</ul>
+		<h4>Children Mini Holiday Special: 80 Eur</h4>
+		<ul>
+			<li>Great as Christmas card for grandmas, grandpas, and any family members or friends</li>
+			<li>Christmas-styled (fairy lights, cute decoration)</li>
+			<li>In my home-studio in Haarlem, really close to the train station</li>
+			<li>30 minutes, real quick </li>
+			<li>10 edited images</li>
+		</ul>
 
+		<h4>Mini session for everybody: 80 Eur</h4>
+		<ul>
+			<li>Perfect for bloggers, vloggers, photographers and for anyone who needs to refresh their profile photo or 'about me' information</li>
+			<li>Best to be done on-location</li>
+			<li>30 minutes </li>
+			<li>10 perfectly edited photos </li>
+		</ul>
 
-<h4>Mini session for everybody: 80 Eur</h4>
-<ul>
-<li>Perfect for bloggers, vloggers, photographers and for anyone who needs to refresh their profile photo or 'about me' information</li>
-<li>Best to be done on-location</li>
-<li>30 minutes </li>
-<li>10 perfectly edited photos </li>
-</ul>
-
-
-
-<p>Delivery time can take up to 7 days.</p>
-
-
-<p>50% discount is only valid for bookings made until 30. 11. 2017.</p>
+		<p>Delivery time can take up to 7 days.</p>
+		<p>50% discount is only valid for bookings made until 30. 11. 2017.</p>
 		<BookNowButton />
 	</Page>
 );
