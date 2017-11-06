9842176678db3cdbb9a8bc04fed0843317f8c067
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-11-06 10:06:44 +0100

    Update CoupleHungary.jsx

--- src/components/pages/CoupleHungary.jsx
+++ src/components/pages/CoupleHungary.jsx
@@ -4,12 +4,12 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
-		<Photo file="couple_shoots_hun/3.jpg"/>
-		<Photo file="couple_shoots_hun/2.jpg"/>
-		<Photo file="couple_shoots_hun/4.jpg"/>
-		<Photo file="couple_shoots_hun/5.jpg"/>
-		<Photo file="couple_shoots_hun/6.jpg"/>
-		<Photo file="couple_shoots_hun/7.jpg"/>
+		<Photo file="couple_shoots_hun/3.jpg" alt="Couple sitting in nature"/>
+		<Photo file="couple_shoots_hun/2.jpg" alt="Couple smiling in nature"/>
+		<Photo file="couple_shoots_hun/4.jpg" alt="Couple sitting by bonfire"/>
+		<Photo file="couple_shoots_hun/5.jpg" alt="Couple sitting by bonfire"/>
+		<Photo file="couple_shoots_hun/6.jpg" alt="Couple laughing by bonfire"/>
+		<Photo file="couple_shoots_hun/7.jpg" alt="Couple looking each other on long street"/>
 		<BookNowButton />
 	</Page>
 );
