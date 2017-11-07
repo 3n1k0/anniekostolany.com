49a04e1898ebd6fea1412084e01c7b696d2bee8e
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-11-07 21:03:45 +0100

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -9,11 +9,9 @@ export default () => (
 		<p>All portrait sessions booked until 30th November (couple, family and baby) are discounted by 50%!</p>
 		<p>For more information and detailed prices please see the following document: <RatesDownloadLink>anniekostolany_rates2017.pdf</RatesDownloadLink></p>
 
-		<h3>Workflow</h3>
-		<p>After you contact me with your idea, we will discuss the location, the clothes I would recommend, the makeup and the possible props, so everything about the styling.</p>
-		<p>The photoshoot will take 90 minutes, I will help you with the posing, and we will have a lot of fun!</p>
-		<p>In 3-4 days I send you a selection of preview images (not the full resolution yet). From these, you will be able to pick 25 of your favourite photos, which I`ll edit to perfection in my signature style.</p>
-		<p>It's really quick and easy.</p>
+	
+		
+		
 		<p>If you have any more questions, feel free to email me at mail@anniekostolany.com or by filling out the contact form.</p>
 		<p>Delivery time can take up to 7 days.</p>
 		<p>Event photography and mini sessions are excluded from promotion. </p>
