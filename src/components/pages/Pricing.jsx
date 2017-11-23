7d21909a84a5a309176567193554be501a5cca74
andormade <hngrhorace@gmail.com>
2017-11-23 17:47:43 +0100

    fix indentation

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -6,23 +6,21 @@ import RatesDownloadLink from '../RatesDownloadLink';
 export default () => (
 	<Page>
 		<h3>Rates</h3>
-		
+
 		<h2>Children Holiday Minis</h2>
 		<p>
 			<ul>
-			<li>Perfect as Christmas card to family members and friends</li>
-			<li>1-3 children in one set</li> 	
-			<li>Christmas-styled (fairy lights, really cute decoration!)</li> 
-			<li>In my home-studio in Haarlem, really close to the train station</li>
-			<li>Relaxed environment</li>
-			<li>30 minutes</li>
-			<li>6 hand edited, high-resolution images</li>
-			</ul></p>
-		
-		<p>For more information and detailed prices please see the following document: <RatesDownloadLink>anniekostolany_rates2017.pdf</RatesDownloadLink></p>
+				<li>Perfect as Christmas card to family members and friends</li>
+				<li>1-3 children in one set</li>
+				<li>Christmas-styled (fairy lights, really cute decoration!)</li>
+				<li>In my home-studio in Haarlem, really close to the train station</li>
+				<li>Relaxed environment</li>
+				<li>30 minutes</li>
+				<li>6 hand edited, high-resolution images</li>
+			</ul>
+		</p>
 
-	
-		
+		<p>For more information and detailed prices please see the following document: <RatesDownloadLink>anniekostolany_rates2017.pdf</RatesDownloadLink></p>
 		
 		<p>If you have any more questions, feel free to email me at mail@anniekostolany.com or by filling out the contact form.</p>
 
