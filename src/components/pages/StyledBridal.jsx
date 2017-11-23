170546b13c81a844472255c546bc25bd2640419b
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-11-23 17:55:38 +0100

    Update StyledBridal.jsx

--- src/components/pages/StyledBridal.jsx
+++ src/components/pages/StyledBridal.jsx
@@ -4,6 +4,27 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
+		
+		<Photo file="styled_bridal/bride1.jpg"/>
+		<Photo file="styled_bridal/bride2.jpg"/>
+		<Photo file="styled_bridal/bride3.jpg"/>
+		<Photo file="styled_bridal/bride4.jpg"/>
+		<Photo file="styled_bridal/bride5.jpg"/>
+		<Photo file="styled_bridal/bride6.jpg"/>
+		<Photo file="styled_bridal/bride7.jpg"/>
+		<Photo file="styled_bridal/bride8.jpg"/>
+		<Photo file="styled_bridal/bride9.jpg"/>
+		<Photo file="styled_bridal/bride10.jpg"/>
+		<Photo file="styled_bridal/bride11.jpg"/>
+		<Photo file="styled_bridal/bride12.jpg"/>
+		<Photo file="styled_bridal/bride13.jpg"/>
+		<Photo file="styled_bridal/bride14.jpg"/>
+		<Photo file="styled_bridal/bride15.jpg"/>
+		<Photo file="styled_bridal/bride16.jpg"/>
+		<Photo file="styled_bridal/bride17.jpg"/>
+		
+		
+		
 		<BookNowButton />
 	</Page>
 );
