d14250715a79187930aafe9a808fb6fb7850d990
Andor Polgar <hngrhorace@gmail.com>
2017-11-25 17:23:04 +0100

    add photos

--- src/components/pages/AutumnsScent.jsx
+++ src/components/pages/AutumnsScent.jsx
@@ -4,6 +4,18 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
+		<Photo file="autumn_scent/1.jpg" alt="" />
+		<Photo file="autumn_scent/2.jpg" alt="" />
+		<Photo file="autumn_scent/3.jpg" alt="" />
+		<Photo file="autumn_scent/4.jpg" alt="" />
+		<Photo file="autumn_scent/5.jpg" alt="" />
+		<Photo file="autumn_scent/6.jpg" alt="" />
+		<Photo file="autumn_scent/7.jpg" alt="" />
+		<Photo file="autumn_scent/8.jpg" alt="" />
+		<Photo file="autumn_scent/9.jpg" alt="" />
+		<Photo file="autumn_scent/10.jpg" alt="" />
+		<Photo file="autumn_scent/11.jpg" alt="" />
+		<Photo file="autumn_scent/13.jpg" alt="" />
 		<BookNowButton />
 	</Page>
 );
