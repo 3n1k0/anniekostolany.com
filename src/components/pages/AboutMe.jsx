d3d84dce2ca350e55e585dcd21f114fe622af2ea
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 17:17:48 +0100

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -4,7 +4,6 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
-		<h2>About me</h2>
 
 		<Photo file="aboutme/anniekostolany.jpg" alt="Annie Kostolany Photographer"/>
 		
