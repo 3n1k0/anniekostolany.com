35aeb6e8ffbaccba8e88ef1b0c33a31099677b7f
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 20:38:38 +0100

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -6,15 +6,16 @@ import RatesDownloadLink from '../RatesDownloadLink';
 export default () => (
 	<Page>
 		
-	<p>I'm so happy to have you here. I hope I can help you to achieve the perfect photos you dream about. These numbers below will give you and idea of the investment for my photography but I am more than happy to send you a detailed document.</p> 
-		<br />All you need to do is just to send me an email to mail@anniekostolany.com or fill out the contact form below.
+	<p>I'm so happy to have you here. I hope I can help you to achieve the perfect photos you dream about. These numbers below will give you and idea of the investment for my photography but I am more than happy to send you a detailed document. 
+		<br />All you need to do is just to send me an email to mail@anniekostolany.com or fill out the contact form below.</p>
 	
-		
-	<br />Portrait sessions start at 190 eur
-	<br />Couple and family sessions start at 190 eur
-	<br />Mini sessions from 90 eur
-	<br />Wedding packages start from 900 eur
-	<br />Photo walk Haarlem & Amsterdam from 90 EUR 
+	<p>	
+		<br />Portrait sessions start at 190 eur
+		<br />Couple and family sessions start at 190 eur
+		<br />Mini sessions from 90 eur
+		<br />Wedding packages start from 900 eur
+		<br />Photo walk Haarlem & Amsterdam from 90 EUR
+	</p>
 		
 		
 		<p>Event rate: 110 eur/hour</p>
