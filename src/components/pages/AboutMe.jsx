6991cfe388c45880476db99dbaf487ea994421d2
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 17:21:47 +0100

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -8,8 +8,7 @@ export default () => (
 		<Photo file="aboutme/anniekostolany.jpg" alt="Annie Kostolany Photographer"/>
 		
 		<p>Hey there!</p>
-<br />Welcome to my website, my name is Annie and I am a Hungarian girl living in the Netherlands.
-<br />I am a self-taught portrait photographer.	
+<br />Welcome to my website, my name is Annie and I am a Hungarian girl living in the Netherlands. I am a self-taught portrait photographer.	
 <br />My inspiration comes from life and from all the beauty that surrounds us, even if we are not aware of it anymore.
 Most of the time I just quietly observe. I wait until light, texture, color and shape add up to create a feeling or an impression.  
 <br />Then I transform it into photographs.
