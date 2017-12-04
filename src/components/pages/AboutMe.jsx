70c48eb89bf1f94c105e30cf0b7e5834d98bbf07
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 16:04:01 +0100

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -8,14 +8,17 @@ export default () => (
 
 		<Photo file="aboutme/anniekostolany.jpg" alt="Annie Kostolany Photographer"/>
 		
-		<p>Hello!</p>
-		<p>My name is Annie, I am a Hungarian girl living in Haarlem, Netherlands. I take photos and and I'm in love with it.</p>
-		<p>I studied literature, which taught me to express my thoughts but I have always been struggled with expressing my feelings. I tried different forms of writing but I always felt somehow limited. It was not the language, it was me. I simply needed something else.</p>
-		<p>I discovered photography in the beginning of 2017 and I immediately knew I found what I was looking for. What you see in my portfolio is my progress, which is still ongoing. I work hard every day on becoming a better photographer.</p>
-		<p>If I had to describe my personality, I would say I am highly sensitive. I laugh too much, talk crazy things, sometimes cry at the news (or just because...) , and I am always ready to make fun of anything, mostly myself. I would adopt all the orphan children and save all the stray animals. But, and I know it might be weird, at the same time I am really outspoken, critical and sarcastic.</p>
-		<p>I appreciate life, all the beauty that surrounds us. This is where my inspiration comes from. I believe that everybody deserves to be loved. Maybe by now you get the idea why I am so attracted to couple shoots and wedding photography.</p>
-		<p>In the past few months I have been travelling. I took photos in Germany, Hungary, France, the Netherlands, and in the States. No matter where I am, I always try to absorb as much as I can. Not only because I think my photography benefits from everything I experience, but also because I have MAJOR wanderlust. Foreign cultures and all corners of this world fascinate me more than anything.</p>
-		<p>The genres I enjoy the most are portraiture and love shoots, but I would love to challenge myself as an event photographer too. For all enquires don't hesitate to email me at mail@anniekostolany.com or by filling out the contact form.</p>
+		<p>Hey there!</p>
+<p>Welcome to my website, my name is Annie and I am a Hungarian girl living in the Netherlands. I am a self-taught portrait photographer.</p>	
+<p>My inspiration comes from life and from all the beauty that surround us, even if we are not aware of it anymore.
+<p>I just observe. I wait until light, texture, color and shape adds up and creates a feeling. 
+Then I transform it into photographs.</p>	
+
+<p>I love travelling with my camera. I took photos in Germany, Hungary, France, the Netherlands, and in the States. No matter where I am, I always absorb as much as I can, because foreign cultures and all corners of this world fascinate me more than anything.</p>
+
+<p>Is it me who you need as a photographer? Let's meet for a coffee and find it out!</p>
+
+<p>For all enquires don't hesitate to email me at mail@anniekostolany.com or by filling out the contact form.</p>
 		
 		<BookNowButton />
 	</Page>
