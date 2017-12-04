f21fbf6f6f3614eb9eb3777e306354ebd6263a7b
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 16:54:17 +0100

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -10,10 +10,10 @@ export default () => (
 		
 		<p>Hey there!</p>
 <p>Welcome to my website, my name is Annie and I am a Hungarian girl living in the Netherlands.</p> 
-<p>I am a self-taught portrait photographer.</p>	
-<p>My inspiration comes from life and from all the beauty that surrounds us, even if we are not aware of it anymore.
-Most of the time I just quietly observe. I wait until light, texture, color and shape adds up and creates a feeling. </p>
-<p>Then I transform it into photographs.</p>	
+<br />I am a self-taught portrait photographer.	
+<br />My inspiration comes from life and from all the beauty that surrounds us, even if we are not aware of it anymore.
+Most of the time I just quietly observe. I wait until light, texture, color and shape adds up and creates a feeling. 
+<br />Then I transform it into photographs.
 
 <p>I love travelling with my camera. I took photos in Germany, Hungary, France, the Netherlands, and in the States. No matter where I am, I always absorb as much as I can, because foreign cultures and all corners of this world fascinate me more than anything.</p>
 
