922f7566268416690f83dcd4ad7e9615877d18f4
Andor Polgar <hngrhorace@gmail.com>
2017-12-04 21:55:04 +0100

    fix indentation

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
 import { Photo, Page } from './Page';
 import BookNowButton from '../BookNowButton';
-import RatesDownloadLink from '../RatesDownloadLink';
 
 export default () => (
 	<Page>
 		<h2>Investment</h2>
-	<p>I'm so happy to have you here. I hope I can help you to achieve the perfect photos you dream about. These numbers below will give you and idea of the investment for my photography but I am more than happy to send you a detailed document. 
-		<br />All you need to do is just to send me an email to mail@anniekostolany.com or fill out the contact form below.</p>
-	
-	<p>	
-		<br />Portrait sessions start at 190 eur
-		<br />Couple and family sessions start at 190 eur
-		<br />Mini sessions from 90 eur
-		<br />Wedding packages start from 900 eur
-		<br />Photo walk Haarlem & Amsterdam from 90 EUR
-	</p>
-		
-		
+		<p>I'm so happy to have you here. I hope I can help you to achieve the perfect photos you dream about. These numbers below will give you and idea of the investment for my photography but I am more than happy to send you a detailed document. <br /> All you need to do is just to send me an email to mail@anniekostolany.com or fill out the contact form below.</p>
+
+		<p>
+			<br />Portrait sessions start at 190 eur
+			<br />Couple and family sessions start at 190 eur
+			<br />Mini sessions from 90 eur
+			<br />Wedding packages start from 900 eur
+			<br />Photo walk Haarlem & Amsterdam from 90 EUR
+		</p>
+
 		<p>Event rate: 110 eur/hour</p>
 
-		
 		<Photo file="cicuka-2.jpg" alt="Christmas cat"/>
 		<h2>Children Holiday Minis</h2>
-		
-			<br />Perfect as Christmas card to family members and friends
-			<br />1-3 children in one set
-			<br />Christmas-styled (fairy lights, really cute decoration!)
-			<br />In my home-studio in Haarlem, really close to the train station
-			<br />Relaxed environment
-			<br />30 minutes
-			<br />6 hand edited, high-resolution images
-			<br />80 eur
-	
-		
-	
-<BookNowButton />
+
+		<br />Perfect as Christmas card to family members and friends
+		<br />1-3 children in one set
+		<br />Christmas-styled (fairy lights, really cute decoration!)
+		<br />In my home-studio in Haarlem, really close to the train station
+		<br />Relaxed environment
+		<br />30 minutes
+		<br />6 hand edited, high-resolution images
+		<br />80 eur
+
+		<BookNowButton />
 	</Page>
 );
