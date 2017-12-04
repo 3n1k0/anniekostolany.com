d1ec053a21ab75ada3b3dfafb689e033c3076bef
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 21:54:44 +0100

    Update PhotoWalkAmsterdam.jsx

--- src/components/pages/PhotoWalkAmsterdam.jsx
+++ src/components/pages/PhotoWalkAmsterdam.jsx
@@ -4,6 +4,15 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
+		
+		<Photo file="photo_walk_amsterdam/1.jpg"/>
+		<Photo file="photo_walk_amsterdam/2.jpg"/>
+		<Photo file="photo_walk_amsterdam/3.jpg"/>
+		<Photo file="photo_walk_amsterdam/4.jpg"/>
+		<Photo file="photo_walk_amsterdam/5.jpg"/>
+		<Photo file="photo_walk_amsterdam/6.jpg"/>
+		<Photo file="photo_walk_amsterdam/7.jpg"/>
+		<Photo file="photo_walk_amsterdam/8.jpg"/>	
 		<BookNowButton />
 	</Page>
 );
