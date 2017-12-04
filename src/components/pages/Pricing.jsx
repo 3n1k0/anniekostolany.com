1ffba7eca8ad8b52e46352d5830c927f19cf3aae
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-04 20:41:45 +0100

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -5,7 +5,7 @@ import RatesDownloadLink from '../RatesDownloadLink';
 
 export default () => (
 	<Page>
-		
+		<h2>investment</h2>
 	<p>I'm so happy to have you here. I hope I can help you to achieve the perfect photos you dream about. These numbers below will give you and idea of the investment for my photography but I am more than happy to send you a detailed document. 
 		<br />All you need to do is just to send me an email to mail@anniekostolany.com or fill out the contact form below.</p>
 	
