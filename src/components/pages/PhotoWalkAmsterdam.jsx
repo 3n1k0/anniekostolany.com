1fb6a41b5771a333c37cc383a21e8ca71f04cff7
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-07 21:38:16 +0100

    Update PhotoWalkAmsterdam.jsx

--- src/components/pages/PhotoWalkAmsterdam.jsx
+++ src/components/pages/PhotoWalkAmsterdam.jsx
@@ -4,13 +4,17 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
+	<p> For most European people Amsterdam is the city of freedom. But besides that, it is utterly romantic too. As soon as you walk through the narrow streets and cross the canals on one of the million charming bridges you will immediately know what I mean. 
+		It is one of my favourite places on Earth, right next to Haarlem on my list. Haarlem is exactly like a mini-Amsterdam but it's easier to get the perfect photos as there are less tourists there. 
+		Leave the selfie stick home and join me for a photo session! Contact me for details and availability. </p>
+		
 		<Photo file="photo_walk_amsterdam/1.jpg"/>
 		<Photo file="photo_walk_amsterdam/2.jpg"/>
+		<Photo file="photo_walk_amsterdam/55.jpg"/>
 		<Photo file="photo_walk_amsterdam/3.jpg"/>
 		<Photo file="photo_walk_amsterdam/4.jpg"/>
-		<Photo file="photo_walk_amsterdam/5.jpg"/>
+		<Photo file="photo_walk_amsterdam/88.jpg"/>
 		<Photo file="photo_walk_amsterdam/6.jpg"/>
-		<Photo file="photo_walk_amsterdam/7.jpg"/>
 		<Photo file="photo_walk_amsterdam/8.jpg"/>
 		<BookNowButton />
 	</Page>
