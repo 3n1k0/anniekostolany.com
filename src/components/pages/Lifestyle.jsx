aea1300534e1d92fcf4ae4049767f7ab60854eea
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2017-12-08 13:31:00 +0100

    Update Lifestyle.jsx

--- src/components/pages/Lifestyle.jsx
+++ src/components/pages/Lifestyle.jsx
@@ -5,12 +5,11 @@ import BookNowButton from '../BookNowButton';
 
 export default () => (
 	<Page>
-		<h2>Lifestyle</h2>
+		<h2>Lifestyle & Family Portraits</h2>
 		<Photo file="coffee.jpg" alt="Girl drinking coffee"/>
 
-		<p>Check out <Link to="/pricing">my rates</Link> to see the amazing deal I figured out for you to make this fall really special!</p>
-
-		<p>Do you have a beautiful family and super cute kids? Let's take some photos of them!</p> <p>The point of lifestyle photography is to capture the happiest everyday moments without too much stressing about planning the shoot. Let's meet in the cosiness of your home or at any other place chosen by you. Picking the location shows your style, your unique taste, your way of living. And what is the most important: it tells your story. <br/> You will have nothing to do, except having fun with the people you love. This way I can promise that your photos will turn out to be really candid and natural.</p>
+		<p>Do you have a beautiful family and super cute kids? Let's take some photos of them! The point of lifestyle photography is to capture the happiest everyday moments without too much stressing about planning the shoot and forcing unnatural poses. Let's meet in the cosiness of your home or at any other place chosen by you. Picking the location shows your style, your unique taste, way of living. And what is the most important: it tells your story. You will have nothing to do, except having fun with the people you love. This way I can promise that your photos will turn out to be really candid and natural.
+		My main locations are Haarlem & Amsterdam but I would be more than happy to travel to you.</p>
 
 		<BookNowButton />
 	</Page>
