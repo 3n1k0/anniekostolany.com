d993a1aead5882c6dc15991044546d3d10d5a79d
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-03 18:10:07 +0100

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -11,23 +11,11 @@ export default () => (
 			<br />Portrait sessions start at 190 eur
 			<br />Couple and family sessions start at 190 eur
 			<br />Mini sessions from 90 eur
+			<br />Photo walk Haarlem & Amsterdam from 150 EUR
 			<br />Wedding packages start from 900 eur
-			<br />Photo walk Haarlem & Amsterdam from 90 EUR
 		</p>
 
 		<p>Event rate: 110 eur/hour</p>
-
-		<Photo file="cicuka-2.jpg" alt="Christmas cat"/>
-		<h2>Children Holiday Minis</h2>
-
-		<br />Perfect as Christmas card to family members and friends
-		<br />1-3 children in one set
-		<br />Christmas-styled (fairy lights, really cute decoration!)
-		<br />In my home-studio in Haarlem, really close to the train station
-		<br />Relaxed environment
-		<br />30 minutes
-		<br />6 hand edited, high-resolution images
-		<br />80 eur
 
 		<BookNowButton />
 	</Page>
