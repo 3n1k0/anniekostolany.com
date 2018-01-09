dfa53a6fa338e4a4084b328be949d7dc55022405
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-09 12:51:32 +0100

    Update LostBride.jsx

--- src/components/pages/LostBride.jsx
+++ src/components/pages/LostBride.jsx
@@ -9,12 +9,12 @@ export default () => (
 			<meta name="description" content="Moody styled beach photoshoot with a beautiful Dutch model." />
 			<title>Lost Bride | Annie Kostolany</title>
 
-			<meta property="og:title" content="Lost Bride stlyed beach photoshoot| Annie Kostolany" />
+			<meta property="og:title" content="Lost Bride | Annie Kostolany" />
 			<meta property="og:description" content="Moody styled beach photoshoot with a beautiful Dutch model." />
 			<meta property="og:image" content="https://anniekostolany.com/images/lost_bride/mermaid.jpg" />
 		</Helmet>
-		<h2> Photo walk Amsterdam & Haarlem </h2>
-		<p> For most European people Amsterdam is t
+		<h2> Lost Bride </h2>
+		<p> Dramatic and moody photoshoot in the cold waves of Zandvoort beach. This is a story of a bride who lost and found herself.</p>
 
 		<Photo file="lost_bride/mermaid.jpg"/>
 		<Photo file="lost_bride/lookingback.jpg"/>
