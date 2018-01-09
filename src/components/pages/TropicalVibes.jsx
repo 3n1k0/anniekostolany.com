9c29b48b31a2dda469bf45de9e623daf86dc9f60
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-09 12:53:01 +0100

    Update TropicalVibes.jsx

--- src/components/pages/TropicalVibes.jsx
+++ src/components/pages/TropicalVibes.jsx
@@ -7,16 +7,16 @@ export default () => (
 	<Page>
 		
 		<Helmet>
-			<meta name="description" content="You can really go wrong with a naturally beautiful model and exotic palm trees." />
+			<meta name="description" content="You cannot really go wrong with a naturally beautiful model and exotic palm trees." />
 			<title>Tropical Vibes | Annie Kostolany</title>
 
 			<meta property="og:title" content="Tropical Vibes | Annie Kostolany" />
-			<meta property="og:description" content="You can really go wrong with a naturally beautiful model and exoctic palm trees." />
+			<meta property="og:description" content="You cannot really go wrong with a naturally beautiful model and exotic palm trees." />
 			<meta property="og:image" content="https://anniekostolany.com/images/tropical_vibes/palmak.jpg" />
 		</Helmet>
 
 		<h2>Tropical Vibes</h2>
-		<p>Zsofia is a naturally beautiful model who barely wore any makeup for these photos. She moved and posed like it was the most natural thing for her and I found it truly amazing. The exotic palm trees are just the perfect match for her southern features.</p>
+		<p>Zsofia is a naturally beautiful model who barely wore any makeup for these photos. She moved and posed like it was the most natural thing for her which I found truly amazing. The tall and graceful palm trees are just the perfect match for her southern features.</p>
 
 		
 		<Photo file="tropical_vibes/kezekfent.jpg"/>
