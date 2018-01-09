ccd960571a5ee311bdf25e21670030cff2aa6979
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-09 14:01:28 +0100

    Update Pricing.jsx

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -16,7 +16,7 @@ export default () => (
 		</Helmet>
 		<h2>Investment</h2>
 		<Photo file="investment/dorinaek.jpg"/>
-		<p>Since you are on this page, I think it's safe to assume that you like my style and you're considering to hire me.  If you are interested in my rates, I can send you a detailed document of my full package prices to answer all the questions you might have right now. <br /> All you need to do for this is just to send me an email to mail@anniekostolany.com or fill out the contact form below.</p>
+		<p>Since you are on this page, I think it's safe to assume that you like my style and you're considering to hire me.  If you are interested in my rates, I can send you a detailed document of my full package prices to answer all the questions you might have right now.</p> <br /> All you need to do for this is just to send me an email to mail@anniekostolany.com or fill out the contact form below.
 		<p>
 			<br />Portrait sessions start at 190 eur
 			<br />Mini sessions from 90 eur
