69e6675467a7c98472cf78c28d17716bb3d2a3b9
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-09 15:15:38 +0100

    Update FashionConfession.jsx

--- src/components/pages/FashionConfession.jsx
+++ src/components/pages/FashionConfession.jsx
@@ -15,7 +15,7 @@ export default () => (
 			<meta property="og:image" content="https://anniekostolany.com/images/tropical_vibes/oszlopok.jpg" />
 		</Helmet>
 		<h2>Fashion Confession</h2>
-		<p> This girl is half Italian, half Greek and full of life. Maybe it's not so obvious from the photos but all of them were taken around and office in the business area of Amsterdam. I think it proves that location is key, but not everything. </p2>
+		<p> This girl is half Italian, half Greek and full of life. Maybe it's not so obvious from the photos but all of them were taken around and office in the business area of Amsterdam. I think it proves that location is key, but not everything. </p>
 		<Photo file="fashion_confession/annasand.jpg"/>
 		<Photo file="fashion_confession/annasand1.jpg"/>
 		<Photo file="fashion_confession/fuggony.jpg"/>
