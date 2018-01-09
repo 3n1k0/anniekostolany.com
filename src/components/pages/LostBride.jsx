c7f946ec17997ece5bede0e82d7cca33583f7bea
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-09 12:38:49 +0100

    Update LostBride.jsx

--- src/components/pages/LostBride.jsx
+++ src/components/pages/LostBride.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Page, Photo } from './Page';
 import BookNowButton from '../BookNowButton';
+import Helmet from 'react-helmet';
 
 export default () => (
 	<Page>
+		<Helmet>
+			<meta name="description" content="Moody styled beach photoshoot with a beautiful Dutch model." />
+			<title>Lost Bride | Annie Kostolany</title>
+
+			<meta property="og:title" content="Lost Bride stlyed beach photoshoot| Annie Kostolany" />
+			<meta property="og:description" content="Moody styled beach photoshoot with a beautiful Dutch model." />
+			<meta property="og:image" content="https://anniekostolany.com/images/lost_bride/mermaid.jpg" />
+		</Helmet>
+		<h2> Photo walk Amsterdam & Haarlem </h2>
+		<p> For most European people Amsterdam is t
+
 		<Photo file="lost_bride/mermaid.jpg"/>
 		<Photo file="lost_bride/lookingback.jpg"/>
 		<Photo file="lost_bride/csokornezo.jpg"/>
