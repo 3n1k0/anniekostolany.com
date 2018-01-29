eea6c0b92cbe031d375beaa132b7b693f99cafd4
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-01-29 23:33:54 +0100

    Update Boudoir.jsx

--- src/components/pages/Boudoir.jsx
+++ src/components/pages/Boudoir.jsx
@@ -3,8 +3,29 @@ import { Page, Photo } from './Page';
 import BookNowButton from '../BookNowButton';
 
 export default () => (
-	<Page title="Boudoir">
-		<p>Lorem ipsum dolor sit amet.</p>
+<Page
+		title="Boudoir session"
+		description="It's the confidence booster every girls should try."
+		image="boudoir/12.jpg"
+	>
+		<h2>Fashion Confession</h2>
+		<p>
+			Book your own boudoir session now!
+		</p>
+		<Photo file="boudoir/1.jpg" />
+		<Photo file="boudoir/2.jpg" />
+		<Photo file="boudoir/3.jpg" />
+		<Photo file="boudoir/4.jpg" />
+		<Photo file="boudoir/5.jpg" />
+		<Photo file="boudoir/6.jpg" />
+		<Photo file="boudoir/7.jpg" />
+		<Photo file="boudoir/8.jpg" />
+		<Photo file="boudoir/9.jpg" />
+	<Photo file="boudoir/10.jpg" />
+	<Photo file="boudoir/11.jpg" />
+	<Photo file="boudoir/12.jpg" />
+	
 		<BookNowButton />
+
 	</Page>
 );
