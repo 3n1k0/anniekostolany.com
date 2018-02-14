8d1aa5290ec20b60bf733d1dff0b33aeb16cecac
andormade <hngrhorace@gmail.com>
2018-02-14 19:06:56 +0100

    added photoset

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const IMAGE_PATH = 'https://anniekostolany.com/images/';
 
 export default styled.img.attrs({
-	src : props => IMAGE_PATH + props.file
+	src: props => IMAGE_PATH + props.file
 })`
 	display: block;
 	padding: 0 0 40px 0;
