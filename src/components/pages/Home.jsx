3bc85b2b80aa0dd10b3e5042ce4582967d88168c
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-02-14 11:57:40 +0100

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,12 +8,11 @@ export default () => (
 		image="home/zsofi.jpg"
 		permalink="/"
 	>
-		<Photo file="home/amanda2.jpg" />
+		<Photo file="home/elso.jpg" />
 		<Photo file="home/zsofi.jpg" />
 		<Photo file="home/amanda6.jpg" />
 		<Photo file="home/11.jpg" />
 		<Photo file="home/jillmermaid.jpg" />
-		<Photo file="home/kady2.jpg" />
 		<Photo file="home/9.jpg" />
 		<BookNowButton />
 	</Page>
