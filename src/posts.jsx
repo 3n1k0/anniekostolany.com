968027a3692de8bdc34f867a7041eb74e4fada07
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-02-15 16:38:17 +0100

    Update posts.jsx

--- src/posts.jsx
+++ src/posts.jsx
@@ -3,5 +3,6 @@ import PhotoWalkAmsterdam from './components/posts/PhotoWalkAmsterdam';
 import PhotoSessionsForValentines from './components/posts/PhotoSessionsForValentines';
 import Marjolein from './components/posts/Marjolein';
 import MalePortraitSession from './components/posts/MalePortraitSession';
+import TropicalVibes from './components/posts/TropicalVibes';
 
-export default [MalePortraitSession, Marjolein, PhotoSessionsForValentines, PhotoWalkAmsterdam];
+export default [TropicalVibes, MalePortraitSession, Marjolein, PhotoSessionsForValentines, PhotoWalkAmsterdam];
