a33416af36777a5949e281158b878931b83b73ca
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-02-18 23:22:35 +0100

    Update Calvins.jsx

--- src/components/posts/Calvins.jsx
+++ src/components/posts/Calvins.jsx
@@ -10,18 +10,20 @@ export default () => (
 		permalink="calvin-portrait-session"
 	>
 		<p>
-			The TZommy Hilfiger HQ in Amsterdam was the perfect venue for a quick portrait session after a long day at the office. Anna put her amazing outfits together as she has perfect styling skills. 
+			The Tommy Hilfiger HQ in Amsterdam was the perfect venue for a quick portrait session after a long day at the office. 
+			Anna styled her own outfits and I think she did a great job. I'm always happy when there is at least a little bit of wind during my shoots because it changes the dynamics dramaticly.
+		
     
 		</p>
 
 		<Photo file="fashion_confession/annasand.jpg" />
-    <Photo file="fashion_confession/calvin1.jpg" />
-    <Photo file="fashion_confession/calvin2.jpg" />
+    	<Photo file="fashion_confession/calvin1.jpg" />
+    	<Photo file="fashion_confession/calvin2.jpg" />
 		<Photo file="fashion_confession/oszlopok.jpg" />
-    <Photo file="fashion_confession/calvin3.jpg" />
+   		<Photo file="fashion_confession/calvin3.jpg" />
 		<Photo file="fashion_confession/lepcso.jpg" />
-    <Photo file="fashion_confession/calvin4.jpg" />
-	  <Photo file="fashion_confession/mezitlab.jpg" />
+   		<Photo file="fashion_confession/calvin4.jpg" />
+	  	<Photo file="fashion_confession/mezitlab.jpg" />
 
     
 	</BlogPost>
