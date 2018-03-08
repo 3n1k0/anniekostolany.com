b2ba53d277159cf164cc91e5312cec43a3b55596
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-03-08 18:43:47 +0100

    Create Boudoir.jsx

---
+++ src/components/posts/Boudoir.jsx
@@ -0,0 +1,40 @@
+import BlogPost from '../BlogPost';
+import Photo from '../Photo';
+import React from 'react';
+
+export default () => (
+	<BlogPost
+		title="All you need to know about boudoir sessions"
+		description="Slightly feminist content."
+		date="2018-03-08"
+		permalink="boudoir-sessions"
+		image="blog/budi.png"
+	>
+		<p>
+		<h1>What is boudoir?</h1>
+    <Photo file="blog/boudoir2.jpg" />
+    <p>According to the Cambridge online dictionary: <i>'a beautifully decorated room used in the past by a woman for sleeping, dressing, relaxing, and entertaining'</i>
+    <br />(In sentence: "The next morning her ladyship was alone in her boudoir." )
+    <p>So as you can see, the word itself reaches way back into the past. Honestly, I hadn't even heard about boudoir until I got into photography. 
+    Later I found out that boudoir photos are usually taken in a bedroom, wearing next to nothing. It has a romantic and slightly erotic vibe, but it always stays classy and artistic.</p>
+    <Photo file="blog/boudoir3.jpg" />
+    <p>Before I tried photographing boudoir, I wasn't completely sure it was something I liked. But a couple sessions later I can tell it is definitely something every girl should try. 
+It boosts your confidence ridiculously and helps A LOT to accept yourself. And why is it important at all?</p>
+    <br />Imagine this: you wake up in the morning, full of confidence and energy, knowing that whatever happens during the day, it won't stop you. Since you already know that you are sexy as hell, it is one less thing you have to worry about, and you can focus on way more important things, like being more successful.
+    <br />I know how much energy it can consume to worry about your appearance because I have been there. And it is a very sad place for many reasons. 
+    <p>It is sad because unfortunately society tells women, that they need to conform to certain standards to be accepted. It is sad because today a woman's worth is defined by her weight and appearance rather than her personality and her achievements.
+    But it really doesn't have to be this way.</p>
+    <p>Although you cannot change what everyone else is thinking, you can definitely change how YOU think. And this is where a boudoir session can help.</p> 
+    <br />Right now you might think it's not for you because you don't look like a model. But let me tell you something: it is not about trying to look like somebody else.</p> 
+    <br />It's about embracing your God-given body as it is, and feeling confident without being ashamed by what you believe your flaws are. That's right, you don't need to look like a model to have a boudoir session. 
+    <br />You don't even have to try to be perfect, or get a ton of makeup and change yourself so much, that even your own mother wouldn't recognize you. And the reason for it is very simple: because you are already more than perfect.  
+<p>Please remember that you only have one life and it wouldn't be fair if you'd spend most of it with worrying about what other people might say or think about you.
+So forget all the bullshit you stress about, drink your wine, shove handfuls of popcorn in your mouth and laugh loudly. Don't be too polite to say "no" whenever you want to, wear too much makeup or do not wear any, apply to the job you are afraid of.
+I mean yeah, people have right to judge you for being confident in your own skin, but you also have the right not to give a fuck about it. </p>
+<p>Oh, and Happy International Women's Day everyone!</p>
+<Photo file="blog/boudoir5.jpg" />
+<Photo file="blog/boudoir6.jpg" />
+<Photo file="blog/boudoir7.jpg" />
+<Photo file="blog/boudoir8.jpg" />
+		
+);
