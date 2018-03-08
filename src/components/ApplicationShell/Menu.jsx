619d3ed88ffb442f67de6d7aa8f266284775000c
andormade <hngrhorace@gmail.com>
2018-03-08 18:44:52 +0100

    design update

--- src/components/ApplicationShell/Menu.jsx
+++ src/components/ApplicationShell/Menu.jsx
@@ -5,7 +5,6 @@ import { Facebook, Instagram } from './SocialIcons';
 
 const Menu = styled.div`
 	background: #000000;
-	border-bottom: 1px solid #000000;
 	display: block;
 	padding: 0;
 
@@ -25,30 +24,20 @@ const MenuItem = styled(Link)`
 	padding: 0 40px;
 	text-decoration: none;
 	text-align: center;
-
-	&:hover {
-		background-color: #000000;
-		color: #ffffff;
-	}
 `;
 
 const Logo = MenuItem.extend`
-	background-color: #000000;
 	background-image: url(https://anniekostolany.com/images/logo_white.png);
 	background-repeat: no-repeat;
 	background-size: 100%;
 	background-position: center center;
 	color: rgba(0, 0, 0, 0);
-
-
-	&:hover {
-		color: rgba(0, 0, 0, 0);
-	}
 `;
 
 export default () => (
 	<Menu>
 		<Logo to="/">Annie Kostolany Photography</Logo>
+		<MenuItem to="/">Home</MenuItem>
 		<MenuItem to="/about-me">About me</MenuItem>
 		<MenuItem to="/investment">Investment</MenuItem>
 		<MenuItem to="/blog">Blog</MenuItem>
