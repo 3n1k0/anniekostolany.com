ffb0c5c8b60cfe37b46ef5ee2d1ce1aaf2df45c8
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-03-20 23:18:21 +0100

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -13,8 +13,8 @@ export default () => (
 	>
 		<PhotoSet>
 			<PrettyLink
-				to="/portraits"
-				title="Lorem ipsum dolor sit amet"
+				to="/blog/2018/03/15/creative-freedom"
+				title="Studio photography"
 				color="black"
 				background="white"
 				image="home/amanda2.jpg"
@@ -24,23 +24,84 @@ export default () => (
 			<Photo file="home/kady2.jpg" />
 		</PhotoSet>
 
+		
+		
 		<PrettyLink
 			to="/portraits"
-			title="Lorem ipsum dolor sit amet"
+			title="Never give up dreaming"
 			color="white"
 			background="black"
 			image="home/zsofi.jpg"
 		>
 			<Photo file="home/zsofi.jpg" />
 		</PrettyLink>
-		<Photo file="home/11.jpg" />
-		<Photo file="home/jillmermaid.jpg" />
+		
+		
+		
+		
+		<PrettyLink
+			to="/blog/2018/02/10/dutch-model-fall-photoshoot"
+			title="Simplicity is key"
+			color="white"
+			background="black"
+			image="home/11.jpg"
+		>
+			<Photo file="home/11.jpg" />
+		</PrettyLink>
+		
+		
+	
+		
+		<PrettyLink
+			to="/forest-fairy-tale"
+			title="Styled bridals"
+			color="white"
+			background="black"
+			image="home/jillmermaid.jpg"
+		>
+			<Photo file="home/jillmermaid.jpg" />
+		</PrettyLink>
+		
+		
+		
+		
 		<PhotoSet>
-			<Photo file="home/amanda6.jpg" />
-			<Photo file="home/amanda3.jpg" />
+			<PrettyLink
+				to="/blog/2018/03/15/creative-freedom"
+				title="Studio photography"
+				color="black"
+				background="white"
+				image="home/amanda3.jpg"
+			>
+				<Photo file="home/amanda3.jpg" />
+			</PrettyLink>
+			<Photo file="home/kady6.jpg" />
 		</PhotoSet>
-		<Photo file="home/9.jpg" />
-		<Photo file="home/menyasszonyok.jpg" />
+		
+
+		
+		<PrettyLink
+			to="/blog/2018/02/10/dutch-model-fall-photoshoot"
+			title="Simplicity is key"
+			color="white"
+			background="black"
+			image="home/9.jpg"
+		>
+			<Photo file="home/9.jpg" />
+		</PrettyLink>
+		
+		
+		<PrettyLink
+			to="/bridal-beauty"
+			title="Styled bridals"
+			color="white"
+			background="black"
+			image="home/menyasszonyok.jpg"
+		>
+			<Photo file="home/menyasszonyok.jpg" />
+		</PrettyLink>
+		
+		
 
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
