2b381eb8237f1ce9013b31e5081910b6a08dae27
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-03-22 19:58:04 +0100

    Create Couples.jsx

---
+++ src/components/posts/Couples.jsx
@@ -0,0 +1,45 @@
+
+import BlogPost from '../BlogPost';
+import Photo from '../Photo';
+import React from 'react';
+
+export default () => (
+	<BlogPost
+		title="Never stop being in love"
+		description="Romantic couple portrait sessions to celebrate love & happiness"
+		date="2018-03-22"
+		permalink="couple-sessions"
+		image="couple_shoots/9.jpg"
+	>
+		<p>
+     I sipmly LOVE to photograph couples. I always seek for the most perfect, authentic moments between them. 
+     If I wait patiently enough, I can capture their love, passion and even respect, in a true, organic form. 
+     I search for these moments on purpose because I believe they make the relationship one of a kind, and really special. I mean you will not find two couples who treat each other exactly the same way, right?
+    It's no secret that most people are terrified of standing in front of the camera and pose. What they don't know is that it is so much easier to be done, when they are not alone.
+     Being there with a partner saves a huge amount of stress. 
+     As a photographer, I am really sensitive to people's insecurities, and I usually spot issues way before we start the whole process. This simply guarantess that my clients will have a great time without fear, embarrassment and awkwardness.  
+		</p>
+    <p>
+      In home sessions are great if you'd like to show your personality on more than one level. Your apartment, choice of color and designs add a lot to the photos. 
+    </p>
+		<Photo file="couple_shoots/1.jpg" />
+		<Photo file="couple_shoots/2.jpg" />
+		<Photo file="couple_shoots/3.jpg" />
+    <p>
+    Location is secondary when it comes to couple photos. No matter where they happen, these moments are just as beautiful.
+    </p>
+		<Photo file="couple_shoots/4.jpg" />
+		<Photo file="couple_shoots/5.jpg" />
+		<Photo file="couple_shoots/6.jpg" />
+    
+   <p>I love when I am able to unleash my creativity and work freely. I am a hopeless romantic soul, so the results are most likely to be the same.
+   </p>
+    <Photo file="couple_shoots/7.jpg" />
+		<Photo file="couple_shoots/8.jpg" />
+		<Photo file="couple_shoots/9.jpg" />
+    <Photo file="couple_shoots/10.jpg" />
+    
+    <p>Treasure the best moments of your relationship with an Amsterdam photo session. Spots are available during the tulip season, which you might not wanna leave out.  
+   Also, another good news: from now on I provide high quality prints and photo albums! Feel free to ask for more details.</p>
+	</BlogPost>
+);
