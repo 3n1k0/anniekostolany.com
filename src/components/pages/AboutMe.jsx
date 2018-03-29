8e2b70d506e6fcf73f022a3149e4ff631151506a
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-03-29 23:41:26 +0200

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -13,10 +13,11 @@ export default () => (
 		<p>Hey there!</p>
 
 		<p>
-			Welcome to my website! My name is Annie, I am Hungarian couple & lifestyle photographer
+			My name is Annie, I am Hungarian couple & lifestyle photographer
 			living in Amsterdam. 
 			<br /> My inspiration comes from my travels and all the amazing people I have met during my journey. 
-			I'm specialized in portrait photography. My style is clean, airy and bright with a romantic touch.
+			I'm specialized in portrait photography.
+			<br />My style is clean, airy and bright with a romantic touch.
 			If this is what you're looking for, I'm happy to send you my package & information guide. 
 			
 		</p>
