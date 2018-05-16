fa03f7c88bdfbd73eabc38257c317f7c7ce88f49
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-05-16 17:01:24 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import AlbumPage from '../AlbumPage';
 import Photo from '../Photo';
-import PhotoSet from '../PhotoSet';
 import CallToActionButton from '../CallToActionButton';
-import PrettyLink from '../PrettyLink';
 
 export default () => (
 	<AlbumPage
@@ -11,93 +9,16 @@ export default () => (
 		image="home/zsofi.jpg"
 		permalink="/"
 	>
-		<PhotoSet>
-			<PrettyLink
-				to="/blog/2018/03/15/creative-freedom"
-				title="Studio photography"
-				color="black"
-				background="white"
-				image="home/amanda2.jpg"
-			>
-				<Photo file="home/amanda2.jpg" />
-			</PrettyLink>
-			<Photo file="home/kady2.jpg" />
-		</PhotoSet>
-
-
-
-		<PrettyLink
-			to="/portraits"
-			title="Never give up dreaming"
-			color="white"
-			background="black"
-			image="home/zsofi.jpg"
-		>
-			<Photo file="home/zsofi.jpg" />
-		</PrettyLink>
-
-
-
-
-		<PrettyLink
-			to="/blog/2018/02/10/dutch-model-fall-photoshoot"
-			title="Simplicity is key"
-			color="white"
-			background="black"
-			image="home/11.jpg"
-		>
-			<Photo file="home/11.jpg" />
-		</PrettyLink>
-
-
-
-
-		<PrettyLink
-			to="/forest-fairy-tale"
-			title="Styled bridals"
-			color="white"
-			background="black"
-			image="home/jillmermaid.jpg"
-		>
-			<Photo file="home/jillmermaid.jpg" />
-		</PrettyLink>
-
-		<PhotoSet>
-			<PrettyLink
-				to="/blog/2018/03/15/creative-freedom"
-				title="Studio photography"
-				color="black"
-				background="white"
-				image="home/amanda3.jpg"
-			>
-				<Photo file="home/amanda3.jpg" />
-			</PrettyLink>
-			<Photo file="home/amanda6.jpg" />
-		</PhotoSet>
-
-		<PrettyLink
-			to="/blog/2018/02/10/dutch-model-fall-photoshoot"
-			title="Simplicity is key"
-			color="white"
-			background="black"
-			image="home/9.jpg"
-		>
-			<Photo file="home/9.jpg" />
-		</PrettyLink>
-
-
-		<PrettyLink
-			to="/bridal-beauty"
-			title="Styled bridals"
-			color="white"
-			background="black"
-			image="home/menyasszonyok.jpg"
-		>
-			<Photo file="home/menyasszonyok.jpg" />
-		</PrettyLink>
-
-
-
+		<Photo file="image="home/amanda2.jpg"" />
+		<Photo file="home/kady2.jpg" />
+		<Photo file="home/zsofi.jpg" />
+		<Photo file="home/11.jpg" />
+		<Photo file="home/jillmermaid.jpg" />
+		<Photo file="home/amanda3.jpg" />
+		<Photo file="home/amanda6.jpg" />
+		<Photo file="home/9.jpg" />
+		<Photo file="home/menyasszonyok.jpg" />
+		<Photo file="home/9.jpg" />
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
 );
