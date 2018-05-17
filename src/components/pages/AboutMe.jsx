989061d01e42e48068ea4046871e70e6c0a62fc2
Andor Polgar <hngrhorace@gmail.com>
2018-05-17 19:13:50 +0200

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -17,7 +17,7 @@ export default () => (
 		Originally I am from Eastern-Europe, which influences my work just as much as nature, feminine beauty and natural light.
 		</p>
 		
-		<p>mail@anniekostolany.com</>
+		<p>mail@anniekostolany.com</p>
 
 		<CallToActionButton to="/contact" text="Book now" />
 	</TextPage>
