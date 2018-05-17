5dd453584c8309092f2d895dd70eb7926b6ced14
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-05-17 13:40:01 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,20 +5,21 @@ import CallToActionButton from '../CallToActionButton';
 
 export default () => (
 	<AlbumPage
-		title="Portrait &amp; Lifestyle Photography in Amsterdam"
+		title="Fashion photographer based in Amsterdam"
 		image="home/zsofi.jpg"
 		permalink="/"
 	>
-		<Photo file="home/amanda2.jpg" />
-		<Photo file="home/kady2.jpg" />
-		<Photo file="home/zsofi.jpg" />
+		<Photo file="home/1.jpg" />
+		<Photo file="home/2.jpg" />
+		<Photo file="home/3.jpg" />
+		<Photo file="home/4.jpg" />
+		<Photo file="home/5.jpg" />
+		<Photo file="home/6.jpg" />
+		<Photo file="home/7.jpg" />
+		<Photo file="home/8.jpg" />
+		<Photo file="home/9.jpg" />
+		<Photo file="home/10.jpg" />
 		<Photo file="home/11.jpg" />
-		<Photo file="home/jillmermaid.jpg" />
-		<Photo file="home/amanda3.jpg" />
-		<Photo file="home/amanda6.jpg" />
-		<Photo file="home/9.jpg" />
-		<Photo file="home/menyasszonyok.jpg" />
-		<Photo file="home/9.jpg" />
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
 );
