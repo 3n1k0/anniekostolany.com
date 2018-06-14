efe160f03a03f16f78ac411ea6694de1d453d16d
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-06-14 16:15:59 +0200

    Update Book1.jsx

--- src/components/pages/Book1.jsx
+++ src/components/pages/Book1.jsx
@@ -10,6 +10,7 @@ export default () => (
 		image="home/zsofi.jpg"
 		permalink="/"
 	>
+		<Photo file="home/0.jpg" />
 		<PhotoSet>
 			<Photo file="home/1.jpg" />
 			<Photo file="home/2.jpg" />
@@ -24,6 +25,7 @@ export default () => (
 			<Photo file="home/10.jpg" />
 			<Photo file="home/11.jpg" />
 		</PhotoSet>
+		<Photo file="home/12.jpg" />
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
 );
