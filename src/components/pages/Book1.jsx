8d95f00a543b1b8a16e792607087bec5884625d0
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-06-21 22:26:25 +0200

    Update Book1.jsx

--- src/components/pages/Book1.jsx
+++ src/components/pages/Book1.jsx
@@ -14,13 +14,12 @@ export default () => (
 			<Photo file="home/1.jpg" />
 			<Photo file="home/2.jpg" />
 		</PhotoSet>
-		<Photo file="home/0.jpg" />
+		<Photo file="home/3.jpg" />
 		<Photo file="home/4.jpg" />
 		<Photo file="home/5.jpg" />
+		<Photo file="home/6.jpg" />
 		<Photo file="home/7.jpg" />
 		<Photo file="home/8.jpg" />
-		<Photo file="home/12.jpg" />
-		<Photo file="home/9.jpg" />
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
 );
