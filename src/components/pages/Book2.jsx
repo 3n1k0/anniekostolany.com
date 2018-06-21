447ed0fd34d88cd1a5d55ca404929db0d51a3c5d
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-06-21 22:57:57 +0200

    Update Book2.jsx

--- src/components/pages/Book2.jsx
+++ src/components/pages/Book2.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import AlbumPage from '../AlbumPage';
 import Photo from '../Photo';
-import CallToActionButton from '../CallToActionButton';
-import PhotoSet from '../PhotoSet';
+
 
 export default () => (
 	<AlbumPage
@@ -11,12 +10,9 @@ export default () => (
 		permalink="/book-2"
 	>
 		<Photo file="book2/1.jpg" />
-		<PhotoSet>
 		<Photo file="book2/2.jpg" />
 		<Photo file="book2/3.jpg" />
-		</PhotoSet>
 		<Photo file="book2/4.jpg" />
-		<Photo file="book2/5.jpg" />
 		<Photo file="book2/6.jpg" />
 		<Photo file="book2/7.jpg" />
 		<Photo file="book2/8.jpg" />
