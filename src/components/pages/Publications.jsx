07344701264cea3109b957f0ae77640d6a698043
andormade <hngrhorace@gmail.com>
2018-07-30 19:42:04 +0200

    add publications page

--- src/components/pages/Publications.jsx
+++ src/components/pages/Publications.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import AlbumPage from '../AlbumPage';
-import Photo from '../Photo';
 import CallToActionButton from '../CallToActionButton';
 
 export default () => (
-	<AlbumPage
-		title="Publications"
-		image="home/3.jpg"
-		permalink="/publications"
-	>
-		<Photo file="home/3.jpg" />
-		<Photo file="home/4.jpg" />
-		<Photo file="home/5.jpg" />
-		<Photo file="home/6.jpg" />
-		<Photo file="home/7.jpg" />
-		<Photo file="home/8.jpg" />
+	<TextPage title="Publications">
+		<p>Lorem ipsum</p>
 		<CallToActionButton to="/contact" text="Book now" />
-	</AlbumPage>
+	</TextPage>
 );
