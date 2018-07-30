1f3bfe52f58b7763ded0ddd1e97f9e0f1b2867a3
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-07-30 20:08:29 +0200

    Update Publications.jsx

--- src/components/pages/Publications.jsx
+++ src/components/pages/Publications.jsx
@@ -4,7 +4,11 @@ import TextPage from '../TextPage';
 
 export default () => (
 	<TextPage title="Publications">
-		<p>Lorem ipsum</p>
+		<p><a href="http://www.magcloud.com/browse/issue/1476055">SALYSÉ Magazine | Vol 4 : No 46 | May 2018</a></p>
+		<p><a href="http://www.magcloud.com/browse/issue/1475992">Elléments Body</a></p>		
+		<p><a href="http://www.magcloud.com/browse/issue/1473216">IMIRAGE Magazine Issue #213</a></p>
+		<p><a href="https://www.facebook.com/anniekostolany/posts/1840483442707815">RISE Magazine July 2018</a></p>
+		
 		<CallToActionButton to="/contact" text="Book now" />
 	</TextPage>
 );
