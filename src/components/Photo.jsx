96253b28018ae98c95f3947a3bad0a1f31ba8ebf
Andor Polgar <hngrhorace@gmail.com>
2018-11-19 18:00:39 +0100

    Update Photo.jsx

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const IMAGE_PATH = 'https://anniekostolany.com/images/';
+const IMAGE_PATH = 'http://anniekostolany.com/images/';
 
 export default styled.img.attrs({
 	src: props => IMAGE_PATH + props.file
