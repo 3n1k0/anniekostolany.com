4fbec2b6cb722a4352408f2a8c69731417217612
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2020-09-10 13:53:43 +0200

    refactoring

---
+++ src/Blog/posts/haarlem.js
@@ -0,0 +1,76 @@
+const haarlem = {
+    slug: "haarlem-the-charming-city-of-light",
+    title: "HAARLEM: THE DUTCH CITY OF COLOUR AND CHARM",
+    date: "2019. 02. 12.",
+    excerpt:
+      "I first visited Haarlem in 2015, April as a tourist. We were living in Germany and decided to see my cousin in Maastricht, as a last minute idea, we wanted to spend a whole weekend in Amsterdam. I started my online research for a hotel, while we stopped at a gas station...",
+    content: `<p>I first visited Haarlem in 2015, April as a tourist. We were living in Germany and decided to see my cousin in Maastricht, as a last minute idea, we wanted to spend a whole weekend in Amsterdam. I started my online research for a hotel, while we stopped at a gas station on the way (it really was last minute). <br /> I immediately realised how expensive hotel reservations were. I randomly found a really cute looking boutique hostel in a close-by city named Haarlem. When I called them, they told us that they only had one room left,  so I asked them to hold it for an hour until we got there. </p>
+      <p>It was dark and raining when we arrived, but I immediately felt the special atmosphere of the quiet little streets, warm orange street lights and tremendous plants all over the city centre. <br />The hostel was one of the most interesting places I've ever been to; It is decorated with so much care and attention to detail, and they serve lovely breakfast so it was impossible not to love.<br /> We spent an amazing weekend in Haarlem, visited Amsterdam, had some space cakes, a lot of cheese and took plenty of photos, so we did all the touristy things we had to. </p>
+      <br />
+      <p>When it was finally time to leave and go back to Germany, I looked at my husband and said 
+      “You know what? I feel like I could live here” and he nodded.</p><br />
+      <p>
+      Because of work commitments, we had to give up our Netherlands dream. Fast forward to 2017, we are sitting in our Los Angeles apartment, when he told me that we could move back to Amsterdam if we wanted to. <br /> As lovely as LA is, I immediately began to pack my luggage and search for flights, cause I felt an irresistible pull  coming from the picturesque street layouts, bridges and canals, millions of bikes, the international environment, the white sand beach and that was just the start. I was really excited to be a part of this atmosphere and I felt more inspired than ever to start our new life (That's about the time I started photography too, by the way). <br /> When we were looking for places to stay, we saw that Haarlem was a little bit cheaper and the connection was still very good with Amsterdam. For example, when I worked in Amsterdam on a daily basis I had to take the train which was in Centraal Station, in less than 15 minutes. Because of the area we chose, I actually traveled less than some of my colleagues that lived in the main city. 
+      </p><br />
+      <p>Haarlem is smaller and fewer tourists visit it. It has all the charm that Amsterdam has but at the same time you feel like you can breathe. It has many cute cafes and restaurants, I will make a list of my favourites and link it here soon.  </p>
+      <br />
+      <p>Recently, I started to schedule more of my photo sessions in Haarlem. I like to meet people right at the train station and walk around together. I have my preferred routes. There aren't many typical must-see things in Haarlem. There is a main square (Grote Markt), a church in the middle (Grote Kerk) and some great museums. But my favourite thing to do is just to wander around and take in the atmosphere.</p>
+      <br />
+      <br />
+      <p>Here are some of my favourite photos I took of tourists visiting Haarlem: </p>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/8b56d4a6-d1ff-4d76-9376-632a3451883a/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/8b56d4a6-d1ff-4d76-9376-632a3451883a/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/b0b97f94-15d2-4309-8402-bf822ebbf2b0/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/b0b97f94-15d2-4309-8402-bf822ebbf2b0/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/9b1709ba-a433-4cad-9b43-d5dbfb337e19/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/9b1709ba-a433-4cad-9b43-d5dbfb337e19/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/7492b378-d3cf-434f-9c6a-3d4d79b76bbf/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/7492b378-d3cf-434f-9c6a-3d4d79b76bbf/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/559a07d4-7a45-4ed9-8d75-937b1f186690/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/559a07d4-7a45-4ed9-8d75-937b1f186690/-/preview/800x1300/-/format/auto/">
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/3ada5c76-854f-4d55-9d3d-8a7aa1446c31/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/3ada5c76-854f-4d55-9d3d-8a7aa1446c31/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/85d39385-e14e-4667-ad31-e2502cead0c2/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/85d39385-e14e-4667-ad31-e2502cead0c2/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/6473f55a-24d9-422e-b15a-cc4df5a87896/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/6473f55a-24d9-422e-b15a-cc4df5a87896/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+      <picture>
+      <source srcset="https://ucarecdn.com/916d6ec3-f827-42c5-bb6c-300e02823035/-/preview/800x1300/-/format/webp/" type="image/webp"/>
+       <img src="https://ucarecdn.com/916d6ec3-f827-42c5-bb6c-300e02823035/-/preview/800x1300/-/format/auto/"/>
+      </picture>
+
+
+      `,
+    leadimage:
+      "https://ucarecdn.com/b0b97f94-15d2-4309-8402-bf822ebbf2b0/-/preview/800x1300/-/format/auto/",
+  }
+
+  export default haarlem
+
+
+
